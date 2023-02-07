9363db2b4c390257e99c7b0c84061f96b478a69e
mariaximenahpg <federico.businessrico@gmail.com>
2023-02-07 20:03:03 +0100

    aggiunge form contattaci

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,10 +1,77 @@
 <script>
-export default {};
+import axios from "axios";
+import { store } from "../store";
+export default {
+  name: "AboutUs",
+  data() {
+    return {
+      store,
+      formData: {
+        name: "",
+        email: "",
+        message: "",
+      },
+    };
+  },
+  methods:{
+    sendEmail(){
+      // console.log(this.formData);
+      axios.post(`${this.store.api_url}/contact-form`, {
+        name: this.formData.name,
+        email: this.formData.email,
+        message: this.formData.message,
+      })
+      .then((res) => {
+          console.log(res.data);
+      });
+    },
+  },
+
+};
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <h5>Contattaci</h5>
+    <div class="container">
+      <form @submit.prevent="sendEmail()">
+        <div class="mt-3">
+          <label for="name">Nome</label>
+          <input
+            class="form-control"
+            id="name"
+            type="text"
+            placeholder="Inserisci il tuo nome"
+            v-model="formData.name"
+          />
+        </div>
+        <div class="mt-3">
+          <label for="email">Email</label>
+          <input
+            class="form-control"
+            id="email"
+            type="email"
+            placeholder="Inserisci la tua email"
+            v-model="formData.email"
+          />
+        </div>
+        <div class="mt-3">
+          <label for="message">Messaggio</label>
+          <textarea
+            class="form-control"
+            id="message"
+            cols="30"
+            rows="10"
+            placeholder="Inserisci il messaggio*"
+            v-model="formData.message"
+          ></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mt-3">
+          Invia messaggio
+        </button>
+      </form>
+    </div>
+    
   </div>
 </template>
 
---
+++ src/store.js
@@ -0,0 +1,4 @@
+import { reactive } from "vue";
+export const store = reactive({
+    api_url: import.meta.env.VITE_BACKEND_URL,
+});
